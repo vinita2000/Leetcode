575f6874f2f0718a8e0e1ce284ebcd12c9049ad5
Vinita <vinitayadavlkw225@gmail.com>
2021-07-12 22:13:20 +0530

    Update Implement strStr().js

--- Problems/Algorithms/Implement strStr().js
+++ Problems/Algorithms/Implement strStr().js
@@ -4,5 +4,24 @@
  * @return {number}
  */
 var strStr = function(haystack, needle) {
-     return haystack.indexOf(needle)
+     // return haystack.indexOf(needle);
+    
+    let flag = -1;
+    const hLen = haystack.length;
+    const nLen = needle.length;
+    
+    if (nLen === 0) return 0;
+    
+    if (nLen > hLen) return -1;
+    
+    if (nLen === hLen){
+        if (haystack !== needle) return -1;
+    }
+    
+    for (let i=0; i<hLen; i++){
+       let temp = haystack.slice(i, i+nLen);
+        if (needle === temp) return i;
+    }
+    
+    return -1;
 };
