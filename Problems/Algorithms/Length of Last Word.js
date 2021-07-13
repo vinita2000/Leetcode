714b486ca02d7f0b4c8587e023f6aa7555e653e1
Vinita <vinitayadavlkw225@gmail.com>
2021-07-13 14:44:43 +0530

    Update Length of Last Word.js

--- Problems/Algorithms/Length of Last Word.js
+++ Problems/Algorithms/Length of Last Word.js
@@ -2,17 +2,32 @@
  * @param {string} s
  * @return {number}
  */
+// var lengthOfLastWord = function(s) {
+    
+//     let count = 0;
+//     s = s.trim();
+//     const str = s.split('').reverse().join('');
+    
+//     console.log('*'+s+'*', '*'+str+'*');
+    
+//     for (char of str){
+//         if (char === ' ') break;
+//         else count += 1;
+//     }
+//     return count;
+// };
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
 var lengthOfLastWord = function(s) {
     
     let count = 0;
     s = s.trim();
-    const str = s.split('').reverse().join('');
+    const str = s.split(' ').reverse();
     
-    console.log('*'+s+'*', '*'+str+'*');
-    
-    for (char of str){
-        if (char === ' ') break;
-        else count += 1;
-    }
+    count = str[0].length;
+
     return count;
 };
